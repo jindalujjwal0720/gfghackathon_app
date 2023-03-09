7cc1b16afc76a3bfac799e1cc18800d85c302b40
vishalshrivastava <vishalshrivastava914419@gmail.com>
2023-03-09 23:37:41 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/presentation/components/doctors_list_tile.dart

--- lib/presentation/components/doctors_list_tile.dart
+++ lib/presentation/components/doctors_list_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:gfghackathon_app/data/models/doctor_model.dart';
 
 class DoctorsListTile extends StatelessWidget {
@@ -7,6 +8,70 @@ class DoctorsListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(doctor.name);
+    return Container(
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(6),
+                    image: DecorationImage(
+                      image: NetworkImage(doctor.profileImageURL),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40,),
+              ],
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(doctor.name),
+                   SizedBox(width: 40,),
+                   Text(doctor.rating.toString()),
+                   Icon(Icons.star,color: Colors.yellow,),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(doctor.speciality),
+              ),
+              Text(doctor.workingArea),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(Icons.currency_rupee,size: 15),
+                  Text(doctor.pricePerAppointment.toString()),
+                  TextButton(onPressed: (){}, child:
+
+                  Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Text("Appointment"),
+                  ),style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                    foregroundColor:MaterialStateProperty.all<Color>(Colors.white)
+                  ),)
+                ],
+              ),
+
+            ],
+          ),
+        ],
+      ),
+    );
+
   }
 }
