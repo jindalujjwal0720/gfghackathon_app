424a91aad152f3d14624726a542326694fcba771
vishalshrivastava <vishalshrivastava914419@gmail.com>
2023-03-12 19:32:06 +0530

    news list tile

--- lib/presentation/components/news_list_tile.dart
+++ lib/presentation/components/news_list_tile.dart
@@ -9,7 +9,53 @@ class NewsListTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(8.0),
-      child: Text(news.title),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+         Padding(
+           padding: const EdgeInsets.all(8.0),
+           child: ClipRRect(
+             borderRadius: BorderRadius.circular(10),
+             child: Image.network(
+               news.imageURL,
+               height: 80.0,
+               width: 80.0,
+               fit: BoxFit.cover,
+             ),
+           ),
+         ),
+         Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: [
+             Row(
+               mainAxisAlignment: MainAxisAlignment.start,
+               mainAxisSize: MainAxisSize.min,
+               children: [
+                 Expanded(
+                   child: Text(
+                     news.title,
+                     style: TextStyle(
+                       fontWeight: FontWeight.bold,
+                       color: Colors.black,
+                       fontSize: 16
+                     ),
+                     textAlign: TextAlign.start,
+                       maxLines: 1,
+                       overflow: TextOverflow.fade
+                   ),
+                 ),
+               ],
+             ),
+
+           ],
+         )
+
+        ],
+      )
     );
   }
 }
